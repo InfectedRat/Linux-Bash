3f63e594cecb4edc61e669f0deab09c61fcec851
Maxim Babichev <birdclock@gmail.com>
2022-04-04 23:27:09 +0300

    change script 0.1

--- ssh-gen.sh
+++ ssh-gen.sh
@@ -1,24 +1,74 @@
 #!/bin/bash
 
-#!/usr/bin/env bash
-#filename admin.sh
-echo "Wellcome to Rassia"
-echo "What you want to do?"
-select task in ADDUSER EXIT
+# Envs
+# ---------------------------------------------------\
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+cd $SCRIPT_PATH
 
-do
-    case $task in
-        ADDUSER)
-            read -p "Введите имя нового пользователя: " user
-            awk -F: '$3 ~ /1[0-9][0-9][0-9]/ {print $1;}' /home
-            useradd -N -g $user
-            read -p "Задайте пароль пользователя $user: " pswd
-            chpasswd <<< "$user:$pswd"
-            [[ $? == 0 ]] && echo "Ползователь создан" && exit 0;;
-        EXIT)
-            echo 'by!'
-            exit 0;;
-        *)
-            echo "What is this?";;
-    esac
-done
+# Vars
+# ---------------------------------------------------\
+ME=`basename "$0"`
+BACKUPS=$SCRIPT_PATH/backups
+SERVER_NAME=`hostname`
+SERVER_IP=$(hostname -I | cut -d' ' -f1)
+LOG=$SCRIPT_PATH/actions.log
+
+gen_pass() {
+  local l=$1
+  [ "$l" == "" ] && l=9
+  tr -dc A-Za-z0-9 < /dev/urandom | head -c ${l} | xargs
+}
+
+create_user() {
+
+    space
+    read -p "Enter user name: " user
+
+    if id -u "$user" >/dev/null 2>&1; then
+        Error "Error" "User $user exists. Try to set another user name."
+    else
+        Info "Info" "User $user will be create.."
+
+        local pass=$(gen_pass)
+
+        if confirm "Promote user to admin? (y/n or enter for n)"; then
+            useradd -m -s /bin/bash -G wheel ${user}
+            echo "%$user ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$user
+        else
+            useradd -m -s /bin/bash ${user}
+        fi
+
+        # set password
+        echo "$user:$pass" | chpasswd
+
+        Info "Info" "User created. Name: $user. Password: $pass"
+        logthis "User created. Name: $user. Password: $pass"
+
+    fi
+    space
+
+}
+
+# Menu user
+  while true
+    do
+        PS3='Please enter your choice: '
+        options=(
+        "Create new user"
+        "Quit"
+        )
+        select opt in "${options[@]}"
+        do
+         case $opt in
+            "Create new user")
+            break
+            ;;
+            "Quit")
+                 Info "Exit" "Bye"
+                 exit
+             ;;
+            *) echo invalid option;;
+         esac
+    done
+   done
